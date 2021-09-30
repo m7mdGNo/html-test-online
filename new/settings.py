84f0628f976e619e5ea4c39aa7fbd7fe2256cf0f
m7mdGNo <m7mdelganainy2001@gmail.com>
2021-09-30 17:52:14 +0200

    'updatederef6'

--- new/settings.py
+++ new/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e@fp08=(=#$e0giazlz&#84%b+#%5+hjj@$*nj=s2x^g7)m+89'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
